90f1175a7d8935b1b654bbdfdcf622428af19830
Jay-flow <kimpetro153@gmail.com>
2022-02-09 21:24:45 +0900

    Upgrade packages

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -2,35 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 
-const darkModeStatusBarColor = SystemUiOverlayStyle(
-  statusBarColor: Colors.black,
-  statusBarIconBrightness: Brightness.light,
-  systemNavigationBarIconBrightness: Brightness.light,
-  statusBarBrightness: Brightness.dark,
-);
-
-const lightModeStatusBarColor = SystemUiOverlayStyle(
-  statusBarColor: Colors.white,
-  statusBarIconBrightness: Brightness.dark,
-  systemNavigationBarIconBrightness: Brightness.dark,
-  statusBarBrightness: Brightness.dark,
-);
-
 class Themes {
   Themes._();
-
-  static void setStatusBarColors() {
-    final schedulerBinding = SchedulerBinding.instance;
-
-    if (schedulerBinding != null) {
-      Brightness brightness = schedulerBinding.window.platformBrightness;
-      bool isDarkMode = brightness == Brightness.dark;
-
-      SystemChrome.setSystemUIOverlayStyle(
-        isDarkMode ? darkModeStatusBarColor : lightModeStatusBarColor,
-      );
-    }
-  }
 
   static final light = ThemeData.light().copyWith(
     // light theme settings
@@ -46,4 +19,31 @@ class Themes {
       systemOverlayStyle: darkModeStatusBarColor,
     ),
   );
+
+  static void setStatusBarColors() {
+    final schedulerBinding = SchedulerBinding.instance;
+
+    if (schedulerBinding != null) {
+      Brightness brightness = schedulerBinding.window.platformBrightness;
+      bool isDarkMode = brightness == Brightness.dark;
+
+      SystemChrome.setSystemUIOverlayStyle(
+        isDarkMode ? darkModeStatusBarColor : lightModeStatusBarColor,
+      );
+    }
+  }
 }
+
+const darkModeStatusBarColor = SystemUiOverlayStyle(
+  statusBarColor: Colors.black,
+  statusBarIconBrightness: Brightness.light,
+  systemNavigationBarIconBrightness: Brightness.light,
+  statusBarBrightness: Brightness.dark,
+);
+
+const lightModeStatusBarColor = SystemUiOverlayStyle(
+  statusBarColor: Colors.white,
+  statusBarIconBrightness: Brightness.dark,
+  systemNavigationBarIconBrightness: Brightness.dark,
+  statusBarBrightness: Brightness.dark,
+);
