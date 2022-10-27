6da91d83a53900230089cda1cbd733ec91fd1e6a
nain93 <rnrb555@gmail.com>
2022-10-27 14:24:33 +0900

    refactor:getx제거후, provider로 대체

--- lib/controllers/count_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/state_manager.dart';
-
-class CountController extends GetxController {
-  RxInt count = 0.obs;
-
-  void increase() => count.value++;
-}
--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,9 +1,8 @@
-import 'package:flutter_seoul/utils/constants.dart';
-import 'package:flutter_seoul/utils/localization.dart';
-import 'package:get/get.dart';
+// import 'package:flutter_seoul/utils/constants.dart';
+// import 'package:flutter_seoul/utils/localization.dart';
 
-void alert(String message) => Get.snackbar(appName, message);
+// void alert(String message) => Get.snackbar(appName, message);
 
-void alertWarning(String message) => Get.snackbar(t("WARNING"), message);
+// void alertWarning(String message) => Get.snackbar(t("WARNING"), message);
 
-void alertError(String message) => Get.snackbar(t("ERROR"), message);
+// void alertError(String message) => Get.snackbar(t("ERROR"), message);
