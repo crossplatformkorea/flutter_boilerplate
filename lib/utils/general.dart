a5da37590cf60c8345085a97dbf133b6f261f4e7
nain93 <rnrb555@gmail.com>
2023-06-11 23:38:19 +0900

    feat: showModalTopSheet 추가, 버전 업데이트

--- lib/utils/general.dart
+++ lib/utils/general.dart
@@ -16,4 +16,27 @@ class General {
       enableDrag: false,
     );
   }
+
+  void showModalTopSheet(BuildContext context, String message, Widget builder) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      transitionDuration: const Duration(milliseconds: 500),
+      barrierLabel: MaterialLocalizations.of(context).dialogLabel,
+      barrierColor: Colors.black.withOpacity(0.5),
+      pageBuilder: (context, _, __) => builder,
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeOut,
+          ).drive(Tween<Offset>(
+            begin: const Offset(0, -1.0),
+            end: Offset.zero,
+          )),
+          child: child,
+        );
+      },
+    );
+  }
 }
