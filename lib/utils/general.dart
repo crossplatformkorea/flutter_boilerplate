77148a72f2d8d28263206bc715cd485febec3fcf
namkyu koo(구남규) <rnrb555@gmail.com>
2023-06-12 00:03:59 +0900

    feat: show modal top sheet (#29)
    * feat: showModalTopSheet 추가, 버전 업데이트

--- lib/utils/general.dart
+++ lib/utils/general.dart
@@ -16,4 +16,42 @@ class General {
       enableDrag: false,
     );
   }
+
+  void showModalTopSheet(BuildContext context, Widget builder,
+      {Color? backgroundColor, double? height}) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      transitionDuration: const Duration(milliseconds: 500),
+      barrierLabel: MaterialLocalizations.of(context).dialogLabel,
+      barrierColor: Colors.black.withOpacity(0.5),
+      pageBuilder: (context, _, __) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: height,
+              width: MediaQuery.of(context).size.width,
+              child: Material(
+                color: backgroundColor ?? Colors.white,
+                child: builder,
+              ),
+            ),
+          ],
+        );
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeOut,
+          ).drive(Tween<Offset>(
+            begin: const Offset(0, -1.0),
+            end: Offset.zero,
+          )),
+          child: child,
+        );
+      },
+    );
+  }
 }
